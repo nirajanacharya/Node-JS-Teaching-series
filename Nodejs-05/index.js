3399c83f4df8cd3159dc669f1c5f086aca5978ed
Nirajan Acharya <nirajan.acharya666@gmail.com>
2023-05-02 20:19:18 +0545

    custom module creation

---
+++ Nodejs-05/index.js
@@ -0,0 +1,4 @@
+const average = require("./mod")
+console.log(average([3,4]))
+console.log('This is index.js')
+
---
+++ Nodejs-05/mod.js
@@ -0,0 +1,11 @@
+console.log('we are creating module')
+
+function average(arr)
+{
+    let sum = 0;
+    array.forEach(element => {
+      sum+=element;  
+    });
+    return sum/arr.length;
+}
+module.exports = average;
